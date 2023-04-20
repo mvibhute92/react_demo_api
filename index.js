917ad679fc1a099c9c099b4566c99741b4649ab2
mvibhute92 <128572795+mvibhute92@users.noreply.github.com>
2023-04-20 11:41:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ app.post("/deletemultiple", async (req, res) => {
 
 
 
-const port = process.env.PORT || 5000;
+const port = 443 || 5000;
 
 server.listen(port, () => {
   console.log("Listening on port " + port);
